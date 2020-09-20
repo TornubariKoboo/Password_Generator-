4d144c8a3b0eca892449dda96c44daeda85ca0a5
Tornubari <t.koboo12@gmail.com>
2020-09-20 16:08:47 -0400

    added requirements for possible values

--- script.js
+++ script.js
@@ -10,8 +10,19 @@ function writePassword() {
 
 }
 function generatePassword() {
-return "This will be replaced by gnerated password.";
+return "This will be replaced by generated password.";
 } 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// setting password length/complexity 
+var complexity = document.getElementById("slider").value
+// possible password values 
+var values = "ABCDEFGHIJLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz12345678910!@#$%^&*()"
+
+var password = "";
+// for loop to choose password characters
+for(var i = 0; i <= complexity; i++){
+  password = password + values.chartAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+}
