7cd7d7f15b3450cf91a1a5bb64da7fdd98176fe0
Tornubari <t.koboo12@gmail.com>
2020-09-21 14:15:26 -0400

    started code over

--- script.js
+++ script.js
@@ -10,19 +10,8 @@ function writePassword() {
 
 }
 function generatePassword() {
-return "This will be replaced by generated password.";
-} 
+  
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// setting password length/complexity 
-var complexity = document.getElementById("slider").value
-// possible password values 
-var values = "ABCDEFGHIJLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz12345678910!@#$%^&*()"
-
-var password = "";
-// for loop to choose password characters
-for(var i = 0; i <= complexity; i++){
-  password = password + values.chartAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
-}
