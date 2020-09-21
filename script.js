d5bbfb4e3c7272b3d5059ac23ea1f94dc142d231
Tornubari <t.koboo12@gmail.com>
2020-09-21 14:58:12 -0400

    added confirm and prompt functions

--- script.js
+++ script.js
@@ -9,9 +9,32 @@ function writePassword() {
   passwordText.value = password;
 
 }
-function generatePassword() {
-  
+
+prompt( "How many characters would you like your password to contain?")
+confirm("click ok to confirm special characters")
+confirm("click ok to confirm numeric characters")
+
+function generatePassword(lessPassword, morePassword) {
+  if (lessPassword > 8 && morePassword < 129) {
+    return Math.floor(Math.random())
+  }
+  return 0;
 }
+
+
+// function generatePassword(a) {
+//     var password;
+//     if ( a > 8) {
+//       return Math.random();
+//     } else if (a < 129) {
+//       return Math.random();
+//     } else {
+//       return undefined
+//     }
+//     return result;
+//   }
+
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
