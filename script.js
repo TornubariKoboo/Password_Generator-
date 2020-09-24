3c5572727a90a30ba027b637e99bf9de3e64c0cc
Tornubari <t.koboo12@gmail.com>
2020-09-24 16:23:28 -0400

    added a while function

--- script.js
+++ script.js
@@ -5,38 +5,52 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+}
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+  var symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-prompt( "How many characters would you like your password to contain?")
-confirm("click ok to confirm special characters")
-confirm("click ok to confirm numeric characters")
-confirm("click ok to confirm upper case")
-confirm("click ok to confirm lower case")
-
-function generatePassword(lessPassword, morePassword) {
-  if (lessPassword > 8 && morePassword < 129) {
-    return Math.floor(Math.random())
+function generatePassword() {
+  // prompt to ask the user how long would they like their password to be
+  var numberCharacter = prompt(
+    "How many characters would you like your password to contain?"
+  );
+  while (numberCharacter < 8 || numberCharacter > 129) {
+   numberCharacter = prompt("Password needs to be between 8 and 129 characters"); 
   }
-  return 0;
-}
-
-
-// function generatePassword(a) {
-//     var password;
-//     if ( a > 8) {
-//       return Math.random();
-//     } else if (a < 129) {
-//       return Math.random();
-//     } else {
-//       return undefined
-//     }
-//     return result;
-//   }
+  var amountEntered = ('' + getRandomLower());
+  for (i = 0; i < numberCharacter; i++) {
+    (getRandomLower() + amountEntered);
+  }  
+  console.log(amountEntered + getRandomLower)
 
   
+    // return (
+    //   getRandomLower() 
+    //   getRandomNumber() 
+    //   getRandomUpper() 
+    //   getRandomSymbol()
+    // );
+    
+
+  // Message to confirm the user is ok with special characters and numeric characters
+  confirm("click ok to confirm special characters");
+  confirm("click ok to confirm numeric characters");
+  // Message to confirm the user is ok with lower case and upper case characters
+  confirm("click ok to confirm upper case");
+  confirm("click ok to confirm lower case");
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
